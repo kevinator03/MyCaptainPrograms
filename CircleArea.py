cfe57a57e39aed2bc7f22679ce429f61844b8762
kevinator03 <106988538+kevinator03@users.noreply.github.com>
2022-06-13 21:22:09 +0530

    Update CircleArea.py

--- CircleArea.py
+++ CircleArea.py
@@ -4,4 +4,4 @@ val = float(input("Input the radius of the circle: "))
 
 area= math.pi*val*val
 
-print("The area of the circle with radius "+str(val)+" is: "+str(area))
+print("The area of the circle with radius ", val ," is: ", area)
